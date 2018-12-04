4dc327df9fd8144567d282194715097458f03335
Michaela DeForest <mdeforest@galileosuite.com>
2018-12-03 22:48:14 -0500

    create searches table

---
+++ database/migrations/2018_12_04_033833_create_searches_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSearchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('searches', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('channel_name');
+            $table->string('platform');
+            $table->string('url');
+            $table->integer('followers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('searches');
+    }
+}
