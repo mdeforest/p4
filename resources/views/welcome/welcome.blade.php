aaed27c3f60804825c0ef44280f6b7ffe2d43bb4
Michaela DeForest <mdeforest@galileosuite.com>
2018-12-12 23:08:07 -0500

    welcome page

--- resources/views/welcome/welcome.blade.php
+++ resources/views/welcome/welcome.blade.php
@@ -4,6 +4,32 @@
 Welcome
 @endsection
 
-@section('content')
+@push('head')
+    <link rel='stylesheet' type='text/css' href='css/modules/confirmation.css'>
+@endpush
 
+@section('content')
+<div class='container-fluid'>
+    <div class='row'>
+        <div class='col text-center'>
+            <h1>Welcome to Social Search!</h1>
+            <p>
+           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et orci est. Vivamus sollicitudin vitae erat
+           sit amet ullamcorper. Etiam erat ante, ullamcorper eget elit in, egestas ultricies justo. In tempor ante at
+           blandit faucibus. Etiam imperdiet ipsum justo, a hendrerit mi gravida quis. Quisque molestie ligula a mi
+           vehicula consectetur. Sed elementum justo nec laoreet congue. Morbi porta ex magna, ut porttitor dui ultrices
+           quis. Mauris massa magna, rhoncus ut lectus quis, tempor tempus nisl. Aliquam quis gravida est.
+
+           Quisque maximus commodo justo, a lacinia diam convallis at. Integer ac est et sem mollis hendrerit. Duis ut
+           pretium lorem. In aliquet molestie orci in cursus. Pellentesque feugiat enim id mauris vehicula, vitae convallis
+           libero vulputate. Nullam ac risus pellentesque, posuere lectus id, porttitor purus. Mauris elementum, libero eu
+           gravida eleifend, augue lacus euismod nulla, in varius nisl massa ut lorem. Nam bibendum vitae lorem et
+           venenatis. Ut consequat id lorem bibendum maximus. Fusce feugiat quis ligula nec porta. Sed imperdiet
+           ullamcorper lacus vitae tristique. Nulla accumsan nunc non augue blandit, in semper nibh scelerisque.
+           Pellentesque viverra porta mi non efficitur. Sed sapien leo, condimentum quis orci ac, congue pharetra neque. Ut
+           consectetur erat a Leo malesuada condimentum. Vivamus auctor eu sapien sit amet fringilla.
+            </p>
+        </div>
+    </div>
+</div>
 @endsection
