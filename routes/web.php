0daf50d41a60ee01a6d443dc89d3dd51924f31eb
Michaela DeForest <mdeforest@galileosuite.com>
2018-12-12 23:02:58 -0500

    Add necessary routes

--- routes/web.php
+++ routes/web.php
@@ -15,22 +15,73 @@
  * Home
  */
 Route::get('/', 'WelcomeController@index');
+Route::get('/welcome', [
+    'middleware' => 'auth',
+    'uses' => 'WelcomeController@welcome'
+]);
+
+/*
+ * Users
+ */
+Route::get('/account', [
+    'middleware' => 'auth',
+    'uses' => 'UserController@account'
+]);
+
 
 /*
  * Searches
  */
-Route::get('/search', 'SearchController@create');
-Route::get('/search-process', 'SearchController@processSearch');
-Route::get('/search/created', 'SearchController@searchCreated');
+Route::get('/search', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@create'
+]);
 
-Route::get('/modify', 'SearchController@modifyIndex');
-Route::get('/modify/{name}', 'SearchController@modify');
-Route::put('/modify-process', 'SearchController@processModify');
-Route::get('/modify/{name}/updated', 'SearchController@modifyUpdated');
+Route::get('/search-process', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@processSearch'
+]);
 
-Route::get('/review', 'SearchController@reviewIndex');
-Route::get('/review/{name}', 'SearchController@review');
+Route::get('/search/created', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@searchCreated'
+]);
 
-Route::delete('/remove', 'SearchController@remove');
+
+Route::get('/modify', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@modifyIndex'
+]);
+
+Route::get('/modify/{name}', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@modify'
+]);
+
+Route::put('/modify-process', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@processModify'
+]);
+
+Route::get('/modify/{name}/updated', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@modifyUpdated'
+]);
+
+
+Route::get('/review', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@reviewIndex'
+]);
+
+Route::get('/review/{name}', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@review'
+]);
+
+Route::delete('/remove', [
+    'middleware' => 'auth',
+    'uses' => 'SearchController@remove'
+]);
 
 Auth::routes();
