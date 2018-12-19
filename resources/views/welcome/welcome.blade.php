a7ff848d3c52165f2ca2a6e008e50d35b168225a
Michaela DeForest <mdeforest@galileosuite.com>
2018-12-19 17:08:50 -0500

    Update content on welcome page

--- resources/views/welcome/welcome.blade.php
+++ resources/views/welcome/welcome.blade.php
@@ -14,20 +14,10 @@ Welcome
         <div class='col text-center'>
             <h1>Welcome to Social Search!</h1>
             <p>
-           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et orci est. Vivamus sollicitudin vitae erat
-           sit amet ullamcorper. Etiam erat ante, ullamcorper eget elit in, egestas ultricies justo. In tempor ante at
-           blandit faucibus. Etiam imperdiet ipsum justo, a hendrerit mi gravida quis. Quisque molestie ligula a mi
-           vehicula consectetur. Sed elementum justo nec laoreet congue. Morbi porta ex magna, ut porttitor dui ultrices
-           quis. Mauris massa magna, rhoncus ut lectus quis, tempor tempus nisl. Aliquam quis gravida est.
-
-           Quisque maximus commodo justo, a lacinia diam convallis at. Integer ac est et sem mollis hendrerit. Duis ut
-           pretium lorem. In aliquet molestie orci in cursus. Pellentesque feugiat enim id mauris vehicula, vitae convallis
-           libero vulputate. Nullam ac risus pellentesque, posuere lectus id, porttitor purus. Mauris elementum, libero eu
-           gravida eleifend, augue lacus euismod nulla, in varius nisl massa ut lorem. Nam bibendum vitae lorem et
-           venenatis. Ut consequat id lorem bibendum maximus. Fusce feugiat quis ligula nec porta. Sed imperdiet
-           ullamcorper lacus vitae tristique. Nulla accumsan nunc non augue blandit, in semper nibh scelerisque.
-           Pellentesque viverra porta mi non efficitur. Sed sapien leo, condimentum quis orci ac, congue pharetra neque. Ut
-           consectetur erat a Leo malesuada condimentum. Vivamus auctor eu sapien sit amet fringilla.
+                Welcome to Social Search! Social Search allows agents, businesspeople, and just about anyone else find new social media influencers and potential clients. Social Search started when a Talent Agent needed a faster way to find new potential stars within the gaming industry. It was taking too long for him to look through potential clients and pick out good ones from millions of possibilities. Social Search now allows him to narrow his potential matches down and focus on the most likely candidates.
+            </p>
+            <p>
+                To get started with Social Search, Create a new Search that will be set up to run at a certain frequency. Over time a list of results will appear in the "Review" tab with links to their social media profiles. We currently only support "Youtube" but hope to support many more platforms in the future.
             </p>
         </div>
     </div>
