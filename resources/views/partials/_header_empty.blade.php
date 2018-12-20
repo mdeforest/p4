7588525b5a42c4c52d37c0e827bc8ac768893479
Michaela DeForest <mdeforest@galileosuite.com>
2018-12-19 22:28:35 -0500

    Fix image tags

--- resources/views/partials/_header_empty.blade.php
+++ resources/views/partials/_header_empty.blade.php
@@ -1,3 +1,3 @@
 <nav class='navbar navbar-expand-lg navbar-light'>
-    <a class='navbar-brand' href='/'><img class='img-nav' src='/images/logo_transparent_title.png'></a>
+    <a class='navbar-brand' href='/'><img class='img-nav' src='/images/logo_transparent_title.png' alt='logo'></a>
 </nav>
